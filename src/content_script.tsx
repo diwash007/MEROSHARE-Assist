f71d52f2bd98980bf96718c9be53e1fc58cab4fc
Diwash Dahal <diwashdahal75@gmail.com>
2023-10-07 18:28:06 +0545

    🧹 Initialize required stuff

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,8 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
+  if (msg.data) {
+    // carry out manipulation
+    console.log(msg.data);
   } else {
-    sendResponse("Color message is none.");
+    sendResponse("No data found.");
   }
 });
